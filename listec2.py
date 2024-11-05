70fa8e905dc0bddf3500ef99cb56a44b5805986a
mathesh12 <122466079+mathesh12@users.noreply.github.com>
2024-11-05 10:44:16 +0530

    listec2.py

--- listec2.py
+++ listec2.py
@@ -2,8 +2,8 @@ import boto3
 
 client = boto3.client('ec2',
 		region_name = 'ap-south-1',
-		aws_access_key_id = 'AKIAQZWM3ITJ6HHJGSFO',
-		aws_secret_access_key = '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')
+		aws_access_key_id = 'AKIA57VDLY3OAZWZHLEV',
+		aws_secret_access_key = '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')
 		
 myec2 = client.describe_instances()
 for printins in myec2['Reservations']:
